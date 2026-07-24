0d70cd61a231397b93cee3e47d6dc91099ad68ec
Cesaralo <ivanalonso201061@outlook.com>
2026-07-23 21:05:32 -0600

    05_super_short_trips.sql

--- 05_super_short_trips.sql
+++ 05_super_short_trips.sql
@@ -3,6 +3,6 @@ SELECT
     ended_at,
     start_station_name,
     end_station_name
-FROM `eng-skyline-475719-c2.Bikes_data.cleaned_bike_data`
+FROM `eng-skyline-475719-c2.Bikes_data.combined_bikes_data`
 WHERE TIMESTAMP_DIFF(ended_at, started_at, SECOND) < 60
   AND start_station_name = end_station_name;
