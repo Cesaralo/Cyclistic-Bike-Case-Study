37ec7e880c1f84d025be87a32faf30955683b005
Cesaralo <ivanalonso201061@outlook.com>
2026-07-23 21:03:27 -0600

    05_super_short_trips.sql

---
+++ 05_super_short_trips.sql
@@ -0,0 +1,8 @@
+SELECT
+    started_at,
+    ended_at,
+    start_station_name,
+    end_station_name
+FROM `eng-skyline-475719-c2.Bikes_data.cleaned_bike_data`
+WHERE TIMESTAMP_DIFF(ended_at, started_at, SECOND) < 60
+  AND start_station_name = end_station_name;
