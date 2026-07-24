84de0ca5960db47eda444081d3640d765ceef5ac
Cesaralo <ivanalonso201061@outlook.com>
2026-07-23 21:05:06 -0600

    06_super_long_trips.sql

---
+++ 06_super_long_trips.sql
@@ -0,0 +1,7 @@
+SELECT
+    started_at,
+    ended_at,
+    start_station_name,
+    end_station_name
+FROM `eng-skyline-475719-c2.Bikes_data.combined_bikes_data`
+WHERE TIMESTAMP_DIFF(ended_at, started_at, SECOND) > 86400
